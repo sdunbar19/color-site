ccd55efb84e964fe7e1f202664b455ebcb7fe532
Sarah Dunbar <sarah@dunbar.xyz>
2024-07-17 17:15:18 +0000

    change background color

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,18 @@ import HSV from './components/HSV.jsx';
 
 function App() {
   return ( 
-    <div> 
-      <h1> Color Analyzer </h1>
-      <h2> Sarah Dunbar, Roulettech Round 1 Interview </h2>
-      <div style={{display: "flex"}}>
-        <Image />
-        <HSV />
-      </div>
-    </div> 
+    <body style={{background: "#808080", height: "100vh"}}>
+      <div> 
+        <div style={{background: "#FFFFFF"}}>
+          <h1> Color Analyzer </h1>
+          <h2> Sarah Dunbar, Roulettech Round 1 Interview </h2>
+        </div>
+        <div style={{display: "flex"}}>
+          <Image />
+          <HSV />
+        </div>
+      </div> 
+    </body>
   ); 
 }
 
